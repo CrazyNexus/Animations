c6be04828a42b0ecd17fb82eb9b67245e2aab896
Thomas Dubiel <td@crazynexus.de>
2016-12-22 10:18:07 +0100

    initial commit

---
+++ Animation/SecondViewController.cs
@@ -0,0 +1,32 @@
+﻿//
+// SecondViewController.cs
+//
+// Created by Thomas Dubiel on 22.12.2016
+// Copyright 2016 Thomas Dubiel. All rights reserved.
+//
+using System;
+
+using UIKit;
+
+namespace Animation
+{
+	public partial class SecondViewController : UIViewController
+	{
+		public SecondViewController() : base("SecondViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
